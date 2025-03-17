7759cd39d8a97a6ff6c5e20da6bdbcafe5421dd0
Aditya Khanna <adityakhanna@Adityas-Mac-mini.local>
2025-03-17 17:26:04 +0530

    check

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -15,3 +15,22 @@ endif;
 add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
 // END ENQUEUE PARENT ACTION
+
+
+function storefront_child_enqueue_styles() {
+    // Get the parent theme version (to prevent cache issues)
+    $parent_style = 'storefront-style'; 
+
+    // Enqueue Parent Theme CSS
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+
+    // Enqueue Child Theme SCSS Compiled CSS
+    wp_enqueue_style(
+        'storefront-child-style', // Handle name
+        get_stylesheet_directory_uri() . '/assets/css/style.css', // Path to your compiled CSS
+        array($parent_style), // Dependencies (loads after parent theme)
+        filemtime(get_stylesheet_directory() . '/assets/css/style.css'), // Prevent caching
+        'all' // Media type
+    );
+}
+add_action('wp_enqueue_scripts', 'storefront_child_enqueue_styles');
