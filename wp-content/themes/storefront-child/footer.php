8169ea311b1fa4b3124c595c0cc09f80c916a95d
vikrant tyagi <vikrant@progryss.com>
2025-03-20 12:32:20 +0530

    Footer Section

--- wp-content/themes/storefront-child/footer.php
+++ wp-content/themes/storefront-child/footer.php
@@ -13,9 +13,33 @@
 	</div><!-- #content -->
 
 	<?php do_action( 'storefront_before_footer' ); ?>
+<div class="upper-footer-main">
+<div class="col-full">
+		<?php if ( is_active_sidebar( 'upper-footer' ) ) : ?>
+    <div class="upper-footer-widgets">
+        <?php dynamic_sidebar( 'upper-footer' ); ?>
+    </div>
+	</div>
+</div>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="col-full">
+<?php endif; ?>
+		<div class="footer-widgets custom-footer-widgets">
+				<div class="footer-widgets-row">
+					<?php for ( $i = 1; $i <= 7; $i++ ) : ?>
+						<div class="col footer-col-<?php echo $i; ?>">
+							<?php if ( is_active_sidebar( 'footer-' . $i ) ) : ?>
+								<?php dynamic_sidebar( 'footer-' . $i ); ?>
+							<?php endif; ?>
+						</div>
+					<?php endfor; ?>
+				</div>
+						<div class="footer-copyright">
+				&copy; <?php echo date('Y'); ?> <a href="/">EED</a>, All Rights Reserved.
+			</div>
+
+			</div>
 
 			<?php
 			/**
@@ -27,10 +51,13 @@
 			do_action( 'storefront_footer' );
 			?>
 
+
 		</div><!-- .col-full -->
 	</footer><!-- #colophon -->
 
 	<?php do_action( 'storefront_after_footer' ); ?>
+
+
 
 </div><!-- #page -->
 
